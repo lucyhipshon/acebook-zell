11640cb5e3fd4550c46312cd3399d3bdc5beb4d0
Zuhair <khanzuhair96@live.co.uk>
2025-08-14 19:47:07 +0100

    WIP: Add author and timestamp to post model and refactor controller + tests to reflect this

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -2,17 +2,47 @@ const Post = require("../models/post");
 const { generateToken } = require("../lib/token");
 
 async function getAllPosts(req, res) {
-  const posts = await Post.find();
+  const posts = await Post.find().populate("author", "email"); // using email for now, dont want to add changes to the user schema that will affect work that other will be doing
   const token = generateToken(req.user_id);
   res.status(200).json({ posts: posts, token: token });
 }
 
 async function createPost(req, res) {
-  const post = new Post(req.body);
-  post.save();
+  try {
+    // input validation
+    if (!req.body.message || req.body.message.trim().length === 0) {
+      return res.status(400).json({
+        error: "message is required and cannot be empty",
+      });
+    }
 
-  const newToken = generateToken(req.user_id);
-  res.status(201).json({ message: "Post created", token: newToken });
+    // Length validaition (matching frontend post requirement)
+    if (req.body.message.length > 200) {
+      return res.status(400).json({
+        error: "message must be 200 characters or less",
+      });
+    }
+
+    const postData = {
+      message: req.body.message.trim(),
+      author: req.user_id, // this is coming from the token checker middleware
+    };
+
+    const post = new Post(postData);
+    await post.save();
+
+    const newToken = generateToken(req.user_id);
+    res.status(201).json({
+      message: "Post created",
+      token: newToken,
+      post: post,
+    });
+  } catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).json({
+      error: "Failed to create post. Please try again",
+    });
+  }
 }
 
 const PostsController = {
--- api/tests/models/post.test.js
+++ api/tests/models/post.test.js
@@ -1,15 +1,30 @@
 require("../mongodb_helper");
 
 const Post = require("../../models/post");
+const User = require("../../models/user");
 
 describe("Post model", () => {
+  let user; // test need user IDs to use as authors, but users don't exit yet so we create this variable
+
   beforeEach(async () => {
     await Post.deleteMany({});
+    await User.deleteMany({});
+    user = new User({
+      email: "test@example.com",
+      password: "password123",
+    });
+    await user.save();
   });
 
-  it("has a message", () => {
-    const post = new Post({ message: "some message" });
+  // create the user for each of the tests
+
+  it("has a message and author", () => {
+    const post = new Post({
+      message: "some message",
+      author: user._id,
+    });
     expect(post.message).toEqual("some message");
+    expect(post.author.toString()).toEqual(user._id.toString());
   });
 
   it("can list all posts", async () => {
@@ -18,7 +33,10 @@ describe("Post model", () => {
   });
 
   it("can save a post", async () => {
-    const post = new Post({ message: "some message" });
+    const post = new Post({
+      message: "some message",
+      author: user._id,
+    });
 
     await post.save();
     const posts = await Post.find();
