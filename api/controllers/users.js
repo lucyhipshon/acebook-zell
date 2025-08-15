a8eba2100400c408557fe62e30a1217826f9d552
elenamurgia <elenamurgia@yahoo.it>
2025-08-15 10:51:00 +0100

    Adds error handling for email address being unique, password length and email format

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,10 +1,27 @@
 const User = require("../models/user");
 
-function create(req, res) {
+async function create(req, res) {
   console.log("signup data:", req.body);
 
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!password || password.length < 8) {
+    console.log("Password must be at least 8 characters.");
+    return res.status(400).json({message: "Password must be at least 8 characters."});
+  }
+
+  const existingEmail = await User.findOne({email});
+  if (existingEmail) {
+    console.log("Email is already in use.");
+    return res.status(400).json({message: "Email is already in use. Try to sign up with another email or log in."})
+  }
+
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (!emailRegex.test(email)) {
+    console.log("Invalid email format.")
+    return res.status(400).json({message: "Invalid email format."})
+  }
 
   const user = new User({ email, password });
   user
