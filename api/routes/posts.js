4d45582b942e6ac6b7c6dd06e617a0a7c85f4a06
elenamurgia <elenamurgia@yahoo.it>
2025-08-18 14:57:08 +0100

    Added the delete post by id route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -6,6 +6,7 @@ const PostsController = require("../controllers/posts");
 router.get("/", PostsController.getAllPosts);
 router.post("/", PostsController.createPost);
 router.get("/:id", PostsController.getPostById);
+router.delete("/:id", PostsController.deletePostById);
 
 
 module.exports = router;
