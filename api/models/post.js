e4ab99f609c677265a6c1ebf5f7b59339f0fcc5a
Louis Johnson <louisj157@gmail.com>
2025-08-19 10:17:19 +0100

    Add likes + likesCount to post schema and corresponding unit tests

--- api/models/post.js
+++ api/models/post.js
@@ -16,13 +16,44 @@ const PostSchema = new mongoose.Schema(
       type: String, // storing image data as base64 encoded --> simple approach for first iteration - can migrate to more robust based on app scaling
       required: false,
     },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      }
+    ]
   },
 
   { timestamps: true } // Automatically adds createdAt and updatedAt fields
 );
+
+PostSchema.virtual("likesCount").get(function () {
+  return Array.isArray(this.likes) ? this.likes.length : 0
+});
+
+PostSchema.set("toJSON", { virtuals: true });
+PostSchema.set("toObject", { virtuals: true });
 
 // We use the Schema to create the Post model. Models are classes which we can
 // use to construct entries in our Database.
 const Post = mongoose.model("Post", PostSchema);
 
 module.exports = Post;
+
+/*
+
+What post should look like in an API response:
+
+{
+  "_id": "POST_ID",
+  "message": "Hello world",
+  "author": "USER_ID",
+  "createdAt": "2025-08-18T12:34:56.000Z",
+  "updatedAt": "2025-08-18T12:34:56.000Z",
+  "likesCount": 3,
+  "likedByCurrentUser": true
+}
+
+*/
