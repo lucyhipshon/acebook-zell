54702bfd0acbfd14081fc1aaf04aa36ff4b50a88
Louis Johnson <louisj157@gmail.com>
2025-08-20 15:36:11 +0100

    add GET and POST services for comments

---
+++ frontend/src/services/comments.js
@@ -0,0 +1,32 @@
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+// GET /posts/:postId/comments
+export async function getComments(token, postId) {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": "application/json",
+    },
+  };
+
+  const response = await fetch(`${BACKEND_URL}/posts/${postId}/comments`, requestOptions);
+  if (response.status !== 200) throw new Error("Unable to fetch comments");
+  return await response.json();
+}
+
+// POST /posts/:postId/comments
+export async function createComment(token, postId, message) {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ message }),
+  };
+
+  const response = await fetch(`${BACKEND_URL}/posts/${postId}/comments`, requestOptions);
+  if (response.status !== 201) throw new Error("Unable to create comment");
+  return await response.json();
+}
