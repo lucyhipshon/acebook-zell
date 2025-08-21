6649f0d783193ba2013f15c755e2b206a28c8b39
elenamurgia <elenamurgia@yahoo.it>
2025-08-21 21:15:29 +0100

    Refactor code

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -98,105 +98,108 @@ function Post(props) {
 
 
 return (
-  <article className="card" key={props.post._id}>
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img 
-                src={(() => {
-                  if (!props.post.author?.profileImage) {
-                    return 'http://localhost:3000/uploads/default.jpg';
-                  }
-                  // This is the change that fixed the bug - Handles inconsistent slashing (/) in profileImage paths
-                  // Saw this on the ProfilePage component
-                  // If profileImage path has slash - use as is / if it doesn't - add it
-                  // Uses a ternary operator e.g. const variable = condition ? valueIfTrue : valueIfFalse;
-                  const profileImagePath = props.post.author.profileImage.startsWith('/') 
-                    ? props.post.author.profileImage 
-                    : '/' + props.post.author.profileImage;
-                  return `http://localhost:3000${profileImagePath}`;
-                })()} 
-                alt={`Profile picture for ${getDisplayName(props.post.author)}`}
-                style={{
-                  width: '48px',
-                  height: '48px',
-                  objectFit: 'cover',
-                  borderRadius: '50%'
-                }}
-              />
-          </figure>
+  <div className="section">
+    <div className="container">
+      <article className="card" key={props.post._id}>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img 
+                    src={(() => {
+                      if (!props.post.author?.profileImage) {
+                        return 'http://localhost:3000/uploads/default.jpg';
+                      }
+                      // This is the change that fixed the bug - Handles inconsistent slashing (/) in profileImage paths
+                      // Saw this on the ProfilePage component
+                      // If profileImage path has slash - use as is / if it doesn't - add it
+                      // Uses a ternary operator e.g. const variable = condition ? valueIfTrue : valueIfFalse;
+                      const profileImagePath = props.post.author.profileImage.startsWith('/') 
+                        ? props.post.author.profileImage 
+                        : '/' + props.post.author.profileImage;
+                      return `http://localhost:3000${profileImagePath}`;
+                    })()} 
+                    alt={`Profile picture for ${getDisplayName(props.post.author)}`}
+                    style={{
+                      width: '48px',
+                      height: '48px',
+                      objectFit: 'cover',
+                      borderRadius: '50%'
+                    }}
+                  />
+              </figure>
+            </div>
+            <div className="media-content">
+                <p className="title is-4 has-text-left">{getDisplayName(props.post.author)}</p>
+                <small className="is-pulled-right has-text-black hover-text-primary has-text-weight-light">{formatTimeAgo(props.post.createdAt)}</small>
+            </div>
         </div>
-        <div className="media-content">
-            <p className="title is-4 has-text-left">{getDisplayName(props.post.author)}</p>
-            <small className="is-pulled-right has-text-black hover-text-primary has-text-weight-light">{formatTimeAgo(props.post.createdAt)}</small>
+        <div className="content has-text-left">
+          <p className="has-text-black hover-text-primary has-text-weight-normal">{props.post.message}</p>
+          <br />
+
         </div>
-    </div>
-    <div className="content has-text-left">
-      <p className="has-text-black hover-text-primary has-text-weight-normal">{props.post.message}</p>
-      <br />
 
-    </div>
-
-    <nav className="level is-pulled-right">
-          <a 
-            className="level-item is-pulled-right"
-            aria-label="open comments"
-            onClick={props.onOpenComments}
-          >
-            <span className="icon is-small">
-              <i className="fa-solid fa-comment" aria-hidden="true"></i>
-            </span>
-          </a>
-          <a className="level-item" aria-label="like">
-            <span className="icon is-small">
-              <i className="fas fa-heart" aria-hidden="true"></i>
-            </span>
-          </a>
-      </nav>
-    </div>
-
-    <nav className="level is-pulled-right">
-              <a className="level-item is-pulled-right" aria-label="reply">
+        <nav className="level is-pulled-right">
+              <a 
+                className="level-item is-pulled-right"
+                aria-label="open comments"
+                onClick={props.onOpenComments}
+              >
                 <span className="icon is-small">
                   <i className="fa-solid fa-comment" aria-hidden="true"></i>
                 </span>
               </a>
-              <a className="level-item"
-                 aria-label="like"
-                 onClick={handleLikeClick}
-                 style={{cursor:"pointer"}}
-                 >
-                <span className="icon is-small" style={{ marginRight: '4px' }}>
-                  <i 
-                    className="fas fa-heart" 
-                    aria-hidden="true"
-                    style={{
-                      color: isLiked ? '#ff3860' : '#3273dc'
-                    }}
-                    ></i>
-                </span>
-                <span style={{fontSize: '0.9rem'}}>
-                  {likeCount}
+              <a className="level-item" aria-label="like">
+                <span className="icon is-small">
+                  <i className="fas fa-heart" aria-hidden="true"></i>
                 </span>
               </a>
           </nav>
         </div>
 
-        {/* Post attachment images */}
-        {props.post.image && (
-          <div className="card-image">
-            <figure className="image is-4by3">
-              <img src={props.post.image} alt={`Image for ${props.post.image}`}/>
-            </figure> 
-          </div>
-        )}
+        <nav className="level is-pulled-right">
+                  <a className="level-item is-pulled-right" aria-label="reply">
+                    <span className="icon is-small">
+                      <i className="fa-solid fa-comment" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a className="level-item"
+                    aria-label="like"
+                    onClick={handleLikeClick}
+                    style={{cursor:"pointer"}}
+                    >
+                    <span className="icon is-small" style={{ marginRight: '4px' }}>
+                      <i 
+                        className="fas fa-heart" 
+                        aria-hidden="true"
+                        style={{
+                          color: isLiked ? '#ff3860' : '#3273dc'
+                        }}
+                        ></i>
+                    </span>
+                    <span style={{fontSize: '0.9rem'}}>
+                      {likeCount}
+                    </span>
+                  </a>
+              </nav>
 
-        {/* Delete Post Button */}
-        <div className="media-right">
-          <DeletePost post={props.post} currentUser={props.currentUser} onDelete={props.onDelete}/>
+            {/* Post attachment images */}
+            {props.post.image && (
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src={props.post.image} alt={`Image for ${props.post.image}`}/>
+                </figure> 
+              </div>
+            )}
+
+            {/* Delete Post Button */}
+            <div className="media-right">
+              <DeletePost post={props.post} currentUser={props.currentUser} onDelete={props.onDelete}/>
+            </div>
+          </article>
         </div>
-      </article>
+      </div>
       )
     }
     
