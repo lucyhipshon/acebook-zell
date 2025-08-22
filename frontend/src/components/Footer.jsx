6a326909e1fe335fae39d3aef8b2fae35e6f1ed0
elenamurgia <elenamurgia@yahoo.it>
2025-08-22 12:01:55 +0100

    Refactor code

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="footer has-background-light">
+        <footer className="footer has-background-light is-fixed-bottom py-5">
             <div className="content has-text-centered">
                 <p>Copyright © 2025 Zell Corporation</p>
             </div>
